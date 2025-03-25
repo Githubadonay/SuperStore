4a88d6d096bce4fdd1023d4b3016680908e4b0d3
adonayaraia02@gmail.com <adonayaraia02@gmail.com>
2025-03-25 15:35:54 -0400

    created a choice

--- src/SuperStore.java
+++ src/SuperStore.java
@@ -12,9 +12,16 @@ public class SuperStore {
 
     }
     public static void addItem(Item[] allItems){
+
+        //get user choice 
+        String choice = JOptionPane.showInputDialog("Enter the type of item you want to add\n1.Electronic Item\n2.Home appliance Item");
         //create the first two item 
+        if (choice.equals("1")){
+            allItems[1] = new ElectronicItem("E001", "CPU",2,1);
+        }else if(choice.equals("2")){
         allItems[0] = new HomeAppItem("H001", "Oven", 100);
-        allItems[1] = new ElectronicItem("E001", "CPU",2,1);
+        }
+
     }
     public static void printItem(Item[] allItems){
         String output = "Item Code | Item Name | Item Price\n";
